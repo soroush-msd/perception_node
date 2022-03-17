0360f17ba891dc01d3fd7d00aa5ab14e1363d229
soroush-msd <mosaddad.soroush@pm.me>
2022-03-18 00:46:34 +1100

    perception state added

--- scripts/launchTest.py
+++ scripts/launchTest.py
@@ -1,34 +1,20 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from email.mime import image
 
 import rospy
-
-# Brings in the SimpleActionClient
 import sys
 import actionlib
-import rospy
-from std_msgs.msg import Int8, String
 import darknet_ros_msgs.msg
 import darknet_ros_msgs
-#from darknet_ros_msgs.action import CheckForObjects
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 import cv2
-
 import roslaunch
 
 bridge = CvBridge()
 
-
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
-import actionlib_tutorials.msg
-
 def darknet_client():
     # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
     client = actionlib.SimpleActionClient('/darknet_ros/check_for_objects', darknet_ros_msgs.msg.CheckForObjectsAction)
 
     # Waits until the action server has started up and started
@@ -36,14 +22,13 @@ def darknet_client():
     client.wait_for_server()
 
     # Creates a goal to send to the action server.
-    cv_message = cv2.imread('/home/soroush/Pictures/dog-puppy-on-garden-royalty-free-image-1586966191.jpg')
+    cv_message = cv2.imread('/home/soroush/Pictures/Yellow-Lab-High-Five.jpg')
     image_message = bridge.cv2_to_imgmsg(cv_message, encoding="rgb8")
     goal = darknet_ros_msgs.msg.CheckForObjectsGoal(image=image_message)
 
     # Sends the goal to the action server.
     client.send_goal(goal)
 
-    #rospy.sleep(15)
     # Waits for the server to finish performing the action.
     client.wait_for_result()
 
@@ -62,13 +47,12 @@ if __name__ == '__main__':
         launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/soroush/catkin_ws/src/darknet_ros/darknet_ros/launch/yolo_v3.launch"])
         launch.start()
         rospy.loginfo("started")
-
-        rospy.sleep(20)
-
+        init = darknet_client()
         result = darknet_client()
-        #rospy.loginfo(result.id)
-        #rospy.loginfo(result.bounding_boxes.bounding_boxes[0].Class)
-        rospy.loginfo(result)
+
+        for i in range(len(result.bounding_boxes.bounding_boxes)):
+            print(result.bounding_boxes.bounding_boxes[i].Class)
+        launch.shutdown()
         
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
--- scripts/stateMachine.py
+++ scripts/stateMachine.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+from time import sleep
 import rospy
 import smach
 import sys
+import roslaunch
+import actionlib
+import darknet_ros_msgs.msg
+import darknet_ros_msgs
+from cv_bridge import CvBridge
+import cv2
+import roslaunch
+
+bridge = CvBridge()
 
 # define state Foo
 class Human(smach.State):
@@ -36,15 +46,68 @@ class Human(smach.State):
 class Robot(smach.State):
     def __init__(self):
         smach.State.__init__(self,
-                            outcomes=['human_turn'],
-                            input_keys=['human_letter_to_robot'])
+                            outcomes=['perception', 'robot_responds_human'],
+                            input_keys=['human_letter_to_robot', 'object_names'])
 
     def execute(self, userdata):
-        print("Robot searching for an object that starts with <<" + userdata.human_letter_to_robot + ">>...")
-        #rospy.loginfo('Executing state ROBOT')
-        return 'human_turn'
-        
 
+        rospy.loginfo('Executing state ROBOT')
+
+        if(len(userdata.object_names) == 0):
+            print("Robot searching for an object that starts with <<" + userdata.human_letter_to_robot + ">>...")
+            return 'perception'
+        
+        else:
+            print("Objects detected are:")
+            for i in range(len(userdata.object_names)):
+                print(userdata.object_names[i])
+        
+        return 'robot_responds_human'
+        
+        
+        
+class Perception(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                            outcomes=['objects'],
+                            input_keys=['human_letter_to_robot', 'objects_detected'])
+
+    def execute(self, userdata):
+        print("Running darknet_ros...")
+        rospy.loginfo('Executing state PERCEPTION')
+
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+
+        launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/soroush/catkin_ws/src/darknet_ros/darknet_ros/launch/yolo_v3.launch"])
+        launch.start()
+        #rospy.loginfo("started")
+        client = actionlib.SimpleActionClient('/darknet_ros/check_for_objects', darknet_ros_msgs.msg.CheckForObjectsAction)
+        client.wait_for_server()
+        cv_message = cv2.imread('/home/soroush/Pictures/Yellow-Lab-High-Five.jpg')
+        image_message = bridge.cv2_to_imgmsg(cv_message, encoding="rgb8")
+        goal = darknet_ros_msgs.msg.CheckForObjectsGoal(image=image_message)
+        client.send_goal(goal)
+        client.wait_for_result()
+        result = client.get_result()
+        #print(result)
+
+        i = 0
+
+        while i < 1:
+            client.send_goal(goal)
+            client.wait_for_result()
+            result = client.get_result()
+            i = i + 1
+
+        for i in range(len(result.bounding_boxes.bounding_boxes)):
+                userdata.objects_detected.append(result.bounding_boxes.bounding_boxes[i].Class)
+                #print()
+
+        #rospy.sleep(20)
+        launch.shutdown()
+
+        return 'objects'
 
 
 # main
@@ -54,7 +117,8 @@ def main():
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['failed', 'successfull'])
     sm.userdata.sm_letter = ""
-    sm.userdata.guess = ""
+    sm.userdata.guess = []
+    #sm.userdata.bridge = CvBridge()
 
     # Open the container
     with sm:
@@ -65,8 +129,15 @@ def main():
                                remapping={'human_letter' : 'sm_letter'})
 
         smach.StateMachine.add('ROBOT', Robot(), 
-                               transitions={'human_turn' : 'successfull'},
-                               remapping={"human_letter_to_robot" : "sm_letter"})
+                               transitions={'perception' : 'PERCEPTION',
+                               "robot_responds_human" : "failed"},
+                               remapping={"human_letter_to_robot" : "sm_letter",
+                               "object_names" : "guess"})
+
+        smach.StateMachine.add('PERCEPTION', Perception(), 
+                               transitions={'objects' : 'ROBOT'},
+                               remapping={"human_letter_to_robot" : "sm_letter",
+                               "objects_detected" : "guess",})
 
     # Execute SMACH plan
     outcome = sm.execute()
